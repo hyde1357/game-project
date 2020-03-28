08a206321c358b6dbda3555a66b86aa7831d919c
cmn97 <christinamh017@gmail.com>
2020-03-28 23:01:04 +0200

    added player controls to cube

---
+++ Assets/MovementControls.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementControls : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 2f;
+    private float currentSpeed = 0f;
+    private float speedSmoothVelocity = 0f;
+    private float speedSmoothTime = 0.1f;
+    private float rotationSpeed = 0.1f;
+    private float gravity = 3f;
+
+    private Transform mainCameraTransform = null;
+
+    private CharacterController controller = null;
+    private Animator animator = null; // Animator not necessary at the moment, but will be once we add animations
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        mainCameraTransform = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        // Vector2 holds x and y values; Vector3 holds x, y, z values.
+        Vector2 movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        Vector3 forward = mainCameraTransform.forward;
+        Vector3 right = mainCameraTransform.right;
+
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 desiredMoveDirection = (forward * movementInput.y + right * movementInput.x).normalized;
+        Vector3 gravityVector = Vector3.zero;
+
+        if(!controller.isGrounded)
+        {
+            gravityVector.y -= gravity; //lower the character if airborn to the ground
+        }
+
+        if(desiredMoveDirection != Vector3.zero)
+        {
+            //Rotates character to face direction.
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection), rotationSpeed);
+        }
+
+        float targetSpeed = movementSpeed * movementInput.magnitude;
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+
+        controller.Move(desiredMoveDirection * currentSpeed * Time.deltaTime);
+
+        controller.Move(gravityVector * Time.deltaTime);
+
+    }
+}
