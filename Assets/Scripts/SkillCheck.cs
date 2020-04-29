9fe91b3e65be15c6124f71d7ddd1f667e5feb34a
hyde1357 <56694315+hyde1357@users.noreply.github.com>
2020-04-29 14:38:31 +0300

    Background music, minor changes for testing
    * Added background music loop
    * Small changes to stat scripts to begin leveling implementation
    * Test objects for xp gain testing
    * Uncommented skill check logging

--- Assets/Scripts/SkillCheck.cs
+++ Assets/Scripts/SkillCheck.cs
@@ -24,10 +24,10 @@ public class SkillCheck
         {
             int roll = dice.Next(1, d);
             total += roll;
-            //Debug.Log("d " + i.ToString() + " value: " + roll.ToString());
+            Debug.Log("d " + i.ToString() + " value: " + roll.ToString());
         }
         total += Mathf.RoundToInt(mod);
-        //Debug.Log("Roll " + dCount.ToString() + "d" + d.ToString() + " + " + mod.ToString() + ". Result: " + total.ToString());
+        Debug.Log("Roll " + dCount.ToString() + "d" + d.ToString() + " + " + mod.ToString() + ". Result: " + total.ToString());
         return total;
     }
 
@@ -38,12 +38,12 @@ public class SkillCheck
         if(total >= difficulty)
         {
             success = true;
-            //Debug.Log("Success!");
+            Debug.Log("Success!");
         }
         else
         {
             success = false;
-            //Debug.Log("Fail!");
+            Debug.Log("Fail!");
         }
         return success;
     }
@@ -52,6 +52,6 @@ public class SkillCheck
     public void AwardXP(float xpGain)
     {
         GameObject.FindGameObjectWithTag("Player").GetComponent<StatSheet>().XP += xpGain;
-        //Debug.Log("Rewarded with " + xpGain.ToString() + " xp. Current xp: " + GameObject.FindGameObjectWithTag("Player").GetComponent<StatSheet>().XP);
+        Debug.Log("Rewarded with " + xpGain.ToString() + " xp. Current xp: " + GameObject.FindGameObjectWithTag("Player").GetComponent<StatSheet>().XP);
     }
 }
